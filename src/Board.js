a4d9a706367154f264dbf6c108ec1fa64e696fcf
Mariya <mariya.podosinova@gmail.com>
2020-05-19 15:54:26 +0100

    Winning the Game

--- src/Board.js
+++ src/Board.js
@@ -81,7 +81,7 @@ class Board extends Component {
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
-    let hasWon = false;
+    let hasWon = board.every((row) => row.every((cell) => !cell));
 
     this.setState({ board, hasWon });
   }
@@ -89,6 +89,9 @@ class Board extends Component {
   /** Render game board or winning message. */
 
   render() {
+    if (this.state.hasWon) {
+      return <h1>You WON!!!</h1>;
+    }
     // if the game is won, just show a winning msg & render nothing else
     // TODO
     // make table board
