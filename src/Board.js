e3c914ff2583d031a56c333cd172f26f04f0168a
Mariya <mariya.podosinova@gmail.com>
2020-05-19 16:31:32 +0100

    Styling with neon

--- src/Board.js
+++ src/Board.js
@@ -90,7 +90,14 @@ class Board extends Component {
 
   render() {
     if (this.state.hasWon) {
-      return <h1>You WON!!!</h1>;
+      return (
+        <div className="Board-title">
+          <div className="winner">
+            <span className="neon-orange">You</span>
+            <span className="neon-blue">Win!</span>
+          </div>
+        </div>
+      );
     }
     // if the game is won, just show a winning msg & render nothing else
     // TODO
@@ -112,9 +119,15 @@ class Board extends Component {
       tblBoard.push(<tr key={y}>{row}</tr>);
     }
     return (
-      <table className="Board">
-        <tbody>{tblBoard}</tbody>
-      </table>
+      <div>
+        <div className="Board-title">
+          <div className="neon-orange">Lights</div>
+          <div className="neon-blue">Out</div>
+        </div>
+        <table className="Board">
+          <tbody>{tblBoard}</tbody>
+        </table>
+      </div>
     );
   }
 }
